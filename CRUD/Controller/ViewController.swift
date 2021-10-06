4759ab0d4707da29b37d7b41cac3af7cc1f4fbb9
alejandrodeleonc <20160338@ce.pucmm.edu.do>
2021-10-06 08:29:35 -0400

    first commit

--- CRUD/Controller/ViewController.swift
+++ CRUD/Controller/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        self.view.addSubview(label)
+        view.backgroundColor = UIColor.lightGray
     }
 
 
---
+++ CRUD/Model/Contact.swift
@@ -0,0 +1,22 @@
+//
+//  Contact.swift
+//  CRUD
+//
+//  Created by Alejandro De León on 5/10/21.
+//
+
+import Foundation
+class Contact {
+    var nombre:String
+    var apellido:String
+    var telefono:String
+    
+    init(nombre: String, apellido: String, telefono:String ){
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+    }
+    
+    
+    
+}
